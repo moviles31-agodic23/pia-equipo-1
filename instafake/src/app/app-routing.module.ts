75b89c3d559a91a0a3c431513b3b3ac26a99cfb5
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-11-05 20:59:02 -0600

    Agregadas rutas necesarias

--- instafake/src/app/app-routing.module.ts
+++ instafake/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { FeedComponent } from './feed/feed.component';
 
 const routes: Routes = [
   {
@@ -12,6 +14,9 @@ const routes: Routes = [
     path: 'registro',
     component: RegistroComponent,
   },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'detallePublicacion/:id', component: PerfilComponent },
+  { path: 'feed', component: FeedComponent },
   {
     path: '',
     redirectTo: 'login',
