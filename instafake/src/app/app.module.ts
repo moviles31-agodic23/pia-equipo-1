12829635879998a518c7b99d5836e07e9f2abd3e
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-11-10 17:01:29 -0600

    Agregada key de firebase a proyecto

--- instafake/src/app/app.module.ts
+++ instafake/src/app/app.module.ts
@@ -6,10 +6,32 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'instafake-b7775',
+        appId: '1:195860332184:web:29c9bde7e64348aedadaed',
+        databaseURL: 'https://instafake-b7775-default-rtdb.firebaseio.com',
+        storageBucket: 'instafake-b7775.appspot.com',
+        apiKey: 'AIzaSyBx-UU86YMd-107T6p-mbA5yDxmvhYVfJs',
+        authDomain: 'instafake-b7775.firebaseapp.com',
+        messagingSenderId: '195860332184',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
