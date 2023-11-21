f212f8ac0ebcb5abc96182994214182e1e7e154e
francisco3104 <emiliano.s3104@gmail.com>
2023-11-20 21:13:32 -0600

    La cague quiero ver en que

--- instafake/src/app/login/login.component.ts
+++ instafake/src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
 import { UserService } from 'src/services/user.service';
 
 @Component({
@@ -12,25 +13,59 @@ import { UserService } from 'src/services/user.service';
   imports: [IonicModule],
 })
 export class LoginComponent implements OnInit {
-  formReg: FormGroup;
+  credentials!: FormGroup;
 
   constructor(
-    private userService: UserService
-  ) {
-  this.formReg = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl
+    private userService: UserService,
+    private fb: FormBuilder,
+    private loadingController: LoadingController,
+    private alertController: AlertController,
+    private router: Router
+  ) {}
+
+  get email(){
+    return this.credentials.get('email');
   }
 
-  )
+  get password(){
+    return this.credentials.get('password');
   }
-  ngOnInit(): void {}
+  ngOnInit(){
+    this.credentials = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
-onsubmit() {
-  this.userService.register(this.formReg.value)
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => console.log(error));
-}
+  async register(){
+    const loading = await this.loadingController.create();
+    await loading.present();
+    const user = await this.userService.register(this.credentials.value);
+    await loading.dismiss();
+    if (user){
+      this.router.navigateByUrl('/home', {replaceUrl: true});
+    } else {
+      this.showAlert('Fallo el Registro', 'Intente de nuevo');
+    }
+  }
+
+  async login(){
+    const loading = await this.loadingController.create();
+    await loading.present();
+    const user = await this.userService.login(this.credentials.value);
+    await loading.dismiss();
+    if (user){
+      this.router.navigateByUrl('/home', {replaceUrl: true});
+    } else {
+      this.showAlert('Fallo el Login', 'Intente de nuevo');
+    }
+  }
+
+  async showAlert(header: any, message: any){
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK']
+    })
+  }
 }
