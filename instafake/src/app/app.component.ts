4c59afb486c9b2bb4f4e067e26c2a1cc5513bce6
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-11-24 13:02:24 -0600

    Botones de la vista creados

--- instafake/src/app/app.component.ts
+++ instafake/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FotosService } from 'src/services/fotos.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  servicioFotos: FotosService = inject(FotosService)
   constructor() {}
 }
--- instafake/src/services/fotos.service.ts
+++ instafake/src/services/fotos.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo, CameraPhoto } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
 
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +14,15 @@ import { Injectable } from '@angular/core';
 export class FotosService {
 
   constructor() { }
+  public async tomarFoto(): Promise<UserPhoto> {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+    return {
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath!
+    }
+  }
 }
