c575895077b91004298cf61cc9e82f18a46bdcc0
re2cc <42478705+re2cc@users.noreply.github.com>
2023-11-25 10:51:54 -0600

    Add android build source Fix multple login buttons

--- instafake/src/environments/environment.prod.ts
+++ instafake/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'instafake-b7775',
+    appId: '1:195860332184:web:29c9bde7e64348aedadaed',
+    databaseURL: 'https://instafake-b7775-default-rtdb.firebaseio.com',
+    storageBucket: 'instafake-b7775.appspot.com',
+    apiKey: 'AIzaSyBx-UU86YMd-107T6p-mbA5yDxmvhYVfJs',
+    authDomain: 'instafake-b7775.firebaseapp.com',
+    messagingSenderId: '195860332184',
+  }
 };
